ee858d5a721a95ebb371ec96ebab56e58900ef2c
Tom Savage <41382678+TomRSavage@users.noreply.github.com>
2019-03-06 13:16:08 +0000

    Add files via upload

---
+++ ParticleSwarm.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 15:59:36 2019
+
+@author: tomsa
+"""
+
+import numpy as np 
+import copy
+import numpy.random as rnd
+import time
+import ParticleSwarmUtility
+
+initiation=ParticleSwarmUtility.initiation
+withinbounds=ParticleSwarmUtility.withinbounds
+Rosenbrock=ParticleSwarmUtility.Rosenbrock
+
+def particleswarm(f,bounds,p,w,c1,c2):
+    '''
+    DESCRIPTION
+    see https://en.wikipedia.org/wiki/Particle_swarm_optimization
+    
+    INPUTS
+    f           :function to be optimized
+    bounds      :bounds of each dimension in form [[x1,x2],[x3,x4]...]
+    p           :number of particles
+    w           :adjustable parameter
+    c1          :adjustable parameter
+    c2          :adjustable parameter
+    
+    OUTPUTS
+    swarm_best  : coordinates of optimal solution, with regards to exit
+                  conditions
+    '''
+    d,particle_pos, particle_best, swarm_best, particle_velocity\
+    = initiation(f,bounds,p) #initializing various arrays
+    old_swarm_best=[0]*d
+    while abs(f(old_swarm_best)-f(swarm_best))>0.000001: #exit condition 
+        for i in range(p): #iterates over each particle
+            rp,rg=rnd.uniform(0,1,2) #creates two random numbers between 0-1
+            particle_velocity[i,:]=w*particle_velocity[i,:]
+            particle_velocity[i,:]+=(c1*rp*(particle_best[i,:]-particle_pos[i,:]))
+            particle_velocity[i,:]+=(c2*rg*(swarm_best[:]-particle_pos[i,:]))
+            #all of the above is regarding updating the particle's velocity
+            #with regards to various parameters (swarm_best, p_best etc..)
+            particle_pos[i,:]+=particle_velocity[i,:] #updating position
+            if withinbounds(bounds,particle_pos[i])==False:
+                particle_velocity[i,:]=0
+            if f(particle_pos[i]) < f(particle_best[i]):
+                particle_best[i,:]=particle_pos[i,:] #checking if new best
+                if f(particle_best[i]) < f(swarm_best): 
+                    old_swarm_best=swarm_best[:]
+                    swarm_best=copy.deepcopy(particle_best[i,:]) 
+                    print('current function value: ',f(swarm_best))
+                    #checking if there's a new swarm best
+    return print('Optimum at: ',swarm_best,'\n','Function at optimum: ',f(swarm_best)) 
+
+
+f=Rosenbrock #setting function to be optimized
+dimensions=5 
+bounds=[0]*dimensions #creating 5 dimensional bounds
+for i in range(dimensions):
+    bounds[i]=[-5,5]
+p=50 #number of particles
+#the three particle swarm parameters:
+w=0.6 
+c1=2.8
+c2=1.3
+
+particleswarm(f,bounds,p,w,c1,c2)
+                
+                
+                
+                
+                
+                
+                
+    
---
+++ ParticleSwarmUtility.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 15:59:36 2019
+
+@author: tomsa
+"""
+
+import numpy as np 
+import copy
+import numpy.random as rnd
+import time
+
+
+def Rosenbrock(X):
+    '''INPUTS
+    X: arguments of the function Rosenbrock
+    OUTPUTS
+    f : evaluation of the Rosenbrock function given the inputs
+    
+    DOMAIN         : Xi is within [-5,10] although can be [-2.048,2.048]
+    DIMENSIONS     : any
+    GLOBAL MINIMUM : f(x)=0 x=[1,...,1] 
+'''
+    f = sum( 100.0*(X[i+1]-X[i]**2)**2 + (1-X[i])**2 for i in range(0,len(X)-1) )
+    return f
+
+def initiation(f,bounds,p):
+    '''
+    INPUTS
+    f       :function to be searched over
+    bounds  :bounds of function in form [[x1,x2],[x3,x4],[x5,x6]...]
+    p       :number of particles
+    
+    OUTPUTS
+    particle_pos      :array of random particle positions 
+    particle_best     :array of best particle positions (same as current)
+    swarm_best        :coordinates of particle with best known position
+    particle_velocity :array of random particle velocity arrays
+    
+    '''
+    d=len(bounds) #finding number of dimensions
+    particle_pos=np.zeros(p) #creating empty position array
+    particle_pos=particle_pos.tolist() #converting array to list
+    particle_velocity=particle_pos[:] #empty velocity array
+    particle_pos_val=particle_pos[:] #empty value array
+    for j in range(p): #iterating ovre the number of particles
+        particle_pos[j]=[rnd.uniform(bounds[i][0],bounds[i][1])\
+                    for i in range(d)] #random coordinate within bounds
+        particle_pos_val[j]=f(particle_pos[j]) #calculating function value
+                                            #at each particle
+        particle_velocity[j]=[rnd.uniform(-abs(bounds[i][1]-bounds[i][0])\
+                    ,abs(bounds[i][1]-bounds[i][0])) for i in range(d)]
+                    #creating random velocity values for each dimension
+    swarm_best=particle_pos[np.argmin(particle_pos_val)]#getting the lowest particle value
+    particle_best=copy.deepcopy(particle_pos)#setting all particles current positions to best
+    return d,np.array(particle_pos), np.array(particle_best), \
+                 np.array(swarm_best), np.array(particle_velocity)
+        
+def withinbounds(bounds,particle_pos):
+    '''
+    DESCRIPTION: 
+        Checks whether a particle's position is within the bounds of the problem 
+        
+    INPUTS
+    bounds      :bounds of problem in form [[x1,x2],[x3,x4]...]
+    particle_pos:coordinates of a particle e.g [p1,p2,p3...]
+    
+    OUTPUTS
+    inbounds    : True if particle is within bounds and false if outside
+    '''
+    inbounds=True
+    for i in range(len(bounds)):
+        if particle_pos[i]<bounds[i][0]:
+            inbounds=False
+        elif particle_pos[i]>bounds[i][1]:
+            inbounds=False
+    return inbounds
+    
+                
+                
+                
+                
+                
+                
+                
+    
